f804273975aa4d2f34e6b985ee4f178344dd3722
Samson Nwokike <samson.ikechukwu@softescu.com>
2020-12-11 14:34:50 +0100

    improve register page design

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -9,7 +9,7 @@ export default function Register() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="w-40 h-screen bg-center bg-cover" style={{ backgroundImage: "url('/images/bricks.jpg')"}}></div>
+      <div className="w-32 h-screen bg-center bg-cover" style={{ backgroundImage: "url('/images/bricks.jpg')"}}></div>
       <div className="flex flex-col justify-center pl-6">
         <div className="w-70">
           <h1 className="mb-2 text-lg font-medium">Sign up</h1>
@@ -22,13 +22,13 @@ export default function Register() {
               <label htmlFor="agreement" className="text-xs cursor-pointer">I agree to get emails about cool stuff on Readit</label>
             </div>
             <div className="mb-2">
-              <input type="email" className="w-full px-3 py-2 bg-gray-100 border-gray-400 rounded" placeholder="Email"/>
+              <input type="email" className="w-full p-3 transition duration-200 border border-gray-300 rounded outline-none bg-gray-50 focus:bg-white hover:bg-white" placeholder="Email"/>
             </div>
             <div className="mb-2">
-              <input type="text" className="w-full px-3 py-2 bg-gray-100 border-gray-400 rounded" placeholder="Username"/>
+              <input type="text" className="w-full p-3 transition duration-200 border border-gray-300 rounded outline-none bg-gray-50 focus:bg-white hover:bg-white" placeholder="Username"/>
             </div>
             <div className="mb-2">
-              <input type="password" className="w-full px-3 py-2 bg-gray-100 border-gray-400 rounded" placeholder="Password"/>
+              <input type="password" className="w-full p-3 transition duration-200 border border-gray-300 rounded outline-none bg-gray-50 ocus:bg-white hover:bg-white" placeholder="Password"/>
             </div>
             <button className="w-full py-2 mb-4 text-xs font-bold text-white uppercase bg-blue-500 border border-blue-500 rounded">Sign Up</button>
           </form>
